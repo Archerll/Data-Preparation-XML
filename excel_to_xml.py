532b85b663529a7155fd9fd42de9393559e2e633
Owen Han <39262093+Archerll@users.noreply.github.com>
2021-01-12 17:57:50 +0800

    Add files via upload

--- excel_to_xml.py
+++ excel_to_xml.py
@@ -6,12 +6,14 @@ import hashlib
 from xml.dom.minidom import Document
 import xml.etree.ElementTree as ET
 from datetime import datetime as dt
-
+import os
+import re
+from tkinter import messagebox
 # 注册命名空间
 ET.register_namespace('sfa', 'urn:oecd:ties:stffatcatypes:v2')
-ET.register_namespace('fta', 'urn:oecd:ties:fatca:v2')
+ET.register_namespace('ftc', 'urn:oecd:ties:fatca:v2')
 sfa = {'sfa': 'urn:oecd:ties:stffatcatypes:v2'}
-fta = {'fta': 'urn:oecd:ties:fatca:v2'}
+fta = {'ftc': 'urn:oecd:ties:fatca:v2'}
 # Example ID
 tin = '2IW3WK.00000.LE.152'
 FATCAEntitySenderId = '000000.00000.TA.840'
@@ -22,6 +24,25 @@ def sta_str(strs):
     return strs[:8]+'-'+strs[8:12]+'-'+strs[12:16]+'-'+strs[16:20]+'-'+strs[20:]
 
 
+# 检测数据字段格式是否合规
+def check_string(strs):
+    string = str(strs)
+    if re.search('[<>\*\&\'\"\#/]', string) or re.search('\-\-', string):
+        print(re.search('[<>\*\&\'\"]', string))
+        return False
+    return True
+
+
+# 检测金额数据是否合规
+def check_curr(currCode, balance , payment):
+    if currCode == 'JPY':
+        if re.search('\.', balance) or re.search('\.', payment):
+            messagebox.showinfo("警告！", "数据的日元金额数值非法 ，请重新执行程序")
+    else:
+        if re.search('\.([0-9])', balance) or re.search('\.([0-9])'):
+            messagebox.showinfo("警告！", "数据的金额数值非法 ，请重新执行程序")
+
+            
 # 获取md5
 def get_md5str(datetime):
     md = hashlib.md5()
@@ -41,7 +62,7 @@ def set_xml(datetime):
     ReportingPeriod.text = datetime.date().strftime('%Y-%m-%d')
     Timestamp = root[0].find('sfa:Timestamp', sfa)
     Timestamp.text = datetime.strftime('%Y-%m-%dT%H:%M:%S')
-    DocRefId = root[1][0][4].find('fta:DocRefId', fta)
+    DocRefId = root[1][0][4].find('ftc:DocRefId', fta)
     DocRefId.text = tin+'.'+get_md5str(datetime)
     # print(root[0][5].tag + ':' + root[0][5].text)
     # print(root[0][6].tag + ':' + root[0][6].text)
@@ -65,10 +86,14 @@ def insert_xml():
     with open('FATCA.xml', 'r+', encoding='utf8') as temp_xml:
         file = temp_xml.readlines()
         print('open info.xml success')
+        file.insert(0, '<?xml version="1.0" encoding="UTF-8"?>')
         # print(file)
         file = insert_info(file, 'FATCA_主动非财务实体_(Active NFE)')
         file = insert_info(file, 'FATCA_控权人_(Controlling Persons)')
-        with open('final_file\\{0}.xml'.format(FATCAEntitySenderId), 'x', encoding='UTF8') as tag_file:
+        path = 'final_file\\{0}_Payload.xml'.format(FATCAEntitySenderId)
+        if os.path.exists(path):
+            os.remove(path)
+        with open(path, 'x', encoding='UTF8') as tag_file:
             for f in file:
                 tag_file.write(f)
         print('End of data write')
@@ -81,18 +106,26 @@ class WriteXml(object):
         self.doc = Document()
         self.datetime = datetime
         # 创建根节点
-        self.root = self.doc.createElement('ReportingGroup')
+        self.root = self.doc.createElement('ftc:ReportingGroup')
         self.doc.appendChild(self.root)
 
     # 读取excel数据
     def get_data_from_excel(self):
-        sheet = pd.read_excel('{0}.xlsx'.format(self.file_name), index_col=None, skiprows=[i for i in range(3)])
-        datas = sheet[0:-2]
-        print('data read successfully')
-        np_datas = datas.to_numpy()
-        # for data in np_datas:
-        #     print(data)
-        #     print(str(data[5])[0:-2])
+        try:
+            sheet = pd.read_excel('{0}.xlsx'.format(self.file_name), index_col=None, skiprows=[i for i in range(3)])
+            datas = sheet[0:-2]
+            print('data read successfully')
+            np_datas = datas.to_numpy()
+            count = 0
+            for datas in np_datas:
+                for data in datas:
+                    if check_string(data) is False:
+                        count += 1
+            if count != 0:
+                messagebox.showinfo("警告！", "{0}数据中存在非法字符 -- 、/* 、 &# ".format(self.file_name))
+            #     print(str(data[5])[0:-2])
+        except IOError as err:
+            print('错误：{err}'.format(err=err))
         return np_datas
         # pass
 
@@ -145,13 +178,13 @@ class WriteXml(object):
             else:
                 break
         for i in range(count):
-            SubstantialOwner = self.doc.createElement('SubstantialOwner')
-            Individual = self.doc.createElement('Individual')
-            TIN = self.doc.createElement('TIN')
+            SubstantialOwner = self.doc.createElement('ftc:SubstantialOwner')
+            Individual = self.doc.createElement('ftc:Individual')
+            TIN = self.doc.createElement('sfa:TIN')
             TIN.appendChild(self.doc.createTextNode(str(data[10+i*3])))
-            Sub_Name = self.doc.createElement('Name')
+            Sub_Name = self.doc.createElement('sfa:Name')
             Sub_Name.appendChild(self.doc.createTextNode(str(data[8+i*3])))
-            Sub_Address = self.doc.createElement('Address')
+            Sub_Address = self.doc.createElement('sfa:Address')
             Sub_Address.appendChild(self.doc.createTextNode(str(data[9+3*i])))
             Individual.appendChild(TIN)
             Individual.appendChild(Sub_Name)
@@ -218,10 +251,13 @@ class WriteXml(object):
             for data in datas:
                 self.create_control_xml(data)
                 count += 1
+        path = "datas\\{0}.xml".format(self.file_name)
+        if os.path.exists(path):
+            os.remove(path)
         try:
-            with open("datas\\{0}.xml".format(self.file_name), 'x+', encoding='UTF8') as file:
+            with open(path, 'x+', encoding='UTF8') as file:
                 self.doc.writexml(file, addindent='\t', newl='\n', encoding='UTF-8')
-                print('data write successfully')
+                print('{0} data write successfully'.format(self.file_name))
         except Exception as err:
             print('错误：{err}'.format(err=err))
 
@@ -231,7 +267,7 @@ if __name__ == '__main__':
     datetime = dt.now()
     set_xml(datetime)
     work1 = WriteXml('FATCA_主动非财务实体_(Active NFE)', datetime)
-    work1.get_data_from_excel()
+    # work1.get_data_from_excel()
     work2 = WriteXml('FATCA_控权人_(Controlling Persons)', datetime)
     work1.xml_group()
     work2.xml_group()
